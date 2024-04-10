10072209addbb9118db461ae0829f37320fbc5f3
Huy Huynh <htqhuy1762@gmail.com>
2024-04-10 16:36:26 +0700

    config env and test API

--- api/server.go
+++ api/server.go
@@ -7,18 +7,20 @@ import (
 
 // Server serves HTTP requests for our banking service.
 type Server struct {
-	store *db.Store
+	store db.Store
 	router *gin.Engine
 }
 
 // NewServer creates a new HTTP server and setup routing.
-func NewServer(store *db.Store) *Server {
+func NewServer(store db.Store) *Server {
 	server := &Server{store: store}
 	router := gin.Default()
 
 	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts/:id", server.getAccount)
 	router.GET("/accounts", server.listAccount)
+	router.PUT("/accounts/:id", server.updateAccount)
+	router.DELETE("/accounts/:id", server.deleteAccount)
 
 	server.router = router
 	return server
